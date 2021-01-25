21ae4e877942423086d65bfb822a4377ba8f0f75
Danny <agyapongdaniel7777@gmail.com>
2021-01-25 17:35:57 +0000

    signuppage style #2

--- src/SignUpPage.jsx
+++ src/SignUpPage.jsx
@@ -1,13 +1,13 @@
 const SignUpPage = () => {
   return (
     //   SignUpPage
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center mb-5">
       <img
-        className="w-44 h-44 "
+        className="w-40  h-32 object-fill"
         src="http://pngimg.com/uploads/amazon/amazon_PNG21.png"
         alt="Logo"
       />
-      <div className="border-gray-200 border-solid border-2 rounded p-5 shadow-sm">
+      <div className="border-gray-200 border-solid border-2 rounded p-5 shadow-sm w-96 ">
         <h1 className=" text-2xl font-medium my-3">Create account</h1>
 
         <div className="flex flex-col mb-2">
@@ -16,7 +16,7 @@ const SignUpPage = () => {
           </label>
           <input
             type="text"
-            className="border-solid border-gray-400 border-2 rounded py-2 px-3  focus: focus  focus:outline-none"
+            className="border-solid border-gray-400 border-2 rounded py-1 px-3  focus:outline-none text-sm"
           />
         </div>
 
@@ -27,7 +27,7 @@ const SignUpPage = () => {
           <input
             type="email"
             name="Email"
-            className="border-solid border-gray-400 border-2 rounded py-2 px-3  focus: focus  focus:outline-none"
+            className="border-solid border-gray-400 border-2 rounded py-1 px-3 focus:outline-none text-sm"
           />
         </div>
 
@@ -39,7 +39,7 @@ const SignUpPage = () => {
             type="password"
             name="Password"
             placeholder="At least 6 characters"
-            className="border-solid border-gray-400 border-2 rounded py-2 px-3  focus: focus  focus:outline-none"
+            className="border-solid border-gray-400 border-2 rounded py-1 px-3 focus:outline-none text-sm"
           />
         </div>
 
@@ -50,17 +50,25 @@ const SignUpPage = () => {
           <input
             type="password"
             name="Password"
-            className="border-solid border-gray-400 border-2 rounded py-2 px-3  focus: focus  focus:outline-none"
+            className="border-solid border-gray-400 border-2 rounded py-1 px-3 focus:outline-none text-sm"
           />
         </div>
 
-        <button className="px-14 py-1 border border-solid border-black bg-yellow-300 text-sm font-semibold rounded mt-2">
+        <button className="px-14 py-1 border border-solid border-black bg-yellow-400 text-sm font-semibold rounded mt-2 w-full">
           Create your Amazon account
         </button>
 
-        <p>
+        <p className="mt-4 text-sm">
           By creating an account, you agree to Amazon's Conditions of Use and
           Privacy Notice.
+        </p>
+
+        <div className=" bg-gray-200  h-0.5 shadow mx-10 my-5"></div>
+
+        <p className="text-sm">Already have an account? Sign-In</p>
+
+        <p className="text-sm">
+          Purchasing for work? Create a business account
         </p>
       </div>
     </div>
