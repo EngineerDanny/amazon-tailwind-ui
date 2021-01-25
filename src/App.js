044187a64426267525b6c1b7032df1d9482a7871
Danny <agyapongdaniel7777@gmail.com>
2021-01-25 02:36:18 +0000

    test files removal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+          <div>Hey</div>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
