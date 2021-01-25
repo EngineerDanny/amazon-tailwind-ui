abece0da9c800580f7cbc1f559e3100444689e33
Danny <agyapongdaniel7777@gmail.com>
2021-01-25 02:47:56 +0000

    update README.md

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-          <div>Hey</div>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
